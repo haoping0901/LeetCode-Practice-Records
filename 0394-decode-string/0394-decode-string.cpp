e191d4de7397e36cb0972666a783755b9ef8505c
haoping0901 <144804410+haoping0901@users.noreply.github.com>
2023-12-03 15:54:14 +0800

    Time: 0 ms (100.00%), Space: 7 MB (52.89%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,48 +1,32 @@
 class Solution {
 public:
     string decodeString(string s) {
-        stack<char> rec;
+        int repeat=0;
         string ret;
+        stack<int> repeats;
+        stack<string> strs;
 
         for (const char& ch: s) {
-            if (ch == ']') {
-                string str;
+            if (isdigit(ch)) {
+                repeat = repeat*10 + (ch - '0');
+            } else if (ch == '[') {
+                repeats.push(repeat);
+                strs.push(ret);
+                ret.clear();
+                repeat = 0;
+            } else if (ch == ']') {
+                    for (int i=repeats.top(); i>0; i--) {
+                        strs.top() += ret;
+                    }
+                    repeats.pop();
 
-                // get the string from stack till '['
-                while (rec.top() != '[') {
-                    str.push_back(rec.top());
-                    rec.pop();
-                }
-                rec.pop();
-
-                // copy them n times
-                int digit=1, repeats=0;
-                while (!rec.empty() && rec.top() >= '0' && rec.top() <= '9') {
-                    repeats += (rec.top()-'0') * digit;
-                    digit *= 10;
-                    rec.pop();
-                }
-
-                string tmp;
-                for (int i=0; i<repeats; i++) {
-                    tmp += str;
-                }
-                reverse(tmp.begin(), tmp.end());
-
-                // push them back to the stack
-                for (const char& t: tmp) {
-                    rec.push(t);
-                }
+                    ret = strs.top();
+                    strs.pop();
             } else {
-                rec.push(ch);
+                ret.push_back(ch);
             }
         }
 
-        while (!rec.empty()) {
-            ret.push_back(rec.top());
-            rec.pop();
-        }
-        reverse(ret.begin(), ret.end());
         return ret;
     }
 };
