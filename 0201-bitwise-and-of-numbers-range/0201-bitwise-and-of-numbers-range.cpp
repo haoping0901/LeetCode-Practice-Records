dc2d45fd5ac579a36a68723bffb7cd8a6b9380dc
haoping0901 <144804410+haoping0901@users.noreply.github.com>
2024-01-04 02:27:11 +0800

    Time: 48 ms (11.29%), Space: 6.5 MB (18.14%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
+        if (!left || !right) return 0;
         if (left == right) return left;
-        int ret, lth = 0, rth = 0;
+        int ret, lth = log2(left), rth = log2(right);
         
         // get the position of leading bit 1 in both left and right
         // left: lth, right: rth
-        // only look up [lth, min(l+1th-1, right)]
-        
-        for (int i=0; i<32; i++) {
-            if ((left>>i) & 1) lth = i;
-            if ((right>>i) & 1) rth = i;
-        }
-
+        // only look up [lth, right)]
         if (lth == rth) {
             ret = left;
             for (long i=left+1; i<=right; i++) ret &= i;
