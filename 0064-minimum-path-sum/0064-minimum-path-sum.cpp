6dfee12663a2c593f45223b7c2f52e704f5e1dad
haoping0901 <144804410+haoping0901@users.noreply.github.com>
2024-01-10 11:23:14 +0800

    Time: 7 ms (73.79%), Space: 9.9 MB (98.25%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,36 +1,16 @@
 class Solution {
-    int m, n;
-    vector<vector<int>> rec;
-    deque<pair<int, int>> q;
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        m = grid.size(), n = grid[0].size();
-        rec = vector<vector<int>>(m, vector<int>(n, 0));
-        rec[0][0] = grid[0][0];
-        q.push_back({0, 0});
+        int m = grid.size(), n = grid[0].size();
 
-        while (!q.empty()) {
-            // cout << "x: " << q.front().first << ", y: " << q.front().second << endl;
-            NextStep(grid);
-            q.pop_front();
+        for (int i=1; i<m; i++) grid[i][0] += grid[i-1][0];
+        for (int i=1; i<n; i++) grid[0][i] += grid[0][i-1];
+
+        for (int i=1; i<m; i++) {
+            for (int j=1; j<n; j++) 
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
         }
 
-        return rec[m-1][n-1];
-    }
-
-    void NextStep(const vector<vector<int>>& grid) {
-        int x = q.front().first, y = q.front().second;
-
-        // down
-        if (y+1 < n && (rec[x][y+1] == 0 || rec[x][y]+grid[x][y+1]<rec[x][y+1])) {
-            q.push_back({x, y+1});
-            rec[x][y+1] = rec[x][y]+grid[x][y+1];
-        }
-
-        // right
-        if (x+1 < m && (rec[x+1][y] == 0 || rec[x][y]+grid[x+1][y]<rec[x+1][y])) {
-            q.push_back({x+1, y});
-            rec[x+1][y] = rec[x][y]+grid[x+1][y];
-        }
+        return grid[m-1][n-1];
     }
 };
