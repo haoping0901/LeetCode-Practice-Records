3a0b9cedba3056b29653421f829524bb51317e01
haoping0901 <144804410+haoping0901@users.noreply.github.com>
2024-01-12 12:07:13 +0800

    Time: 0 ms (100.00%), Space: 8.1 MB (77.01%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -2,25 +2,22 @@ class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         if (obstacleGrid[0][0]) return 0;
+
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-        vector<vector<int>> rec(m, vector<int>(n, 0));
+        vector<int> rec(n, 0);
+        if (!obstacleGrid[0][0]) rec[0] = 1;
 
         for (int i=0; i<m; i++) {
-            if (obstacleGrid[i][0])
-                break;
-            rec[i][0] = 1;
-        }
-        for (int j=0; j<n; j++) {
-            if (obstacleGrid[0][j])
-                break;
-            rec[0][j] = 1;
-        }
+            if (obstacleGrid[i][0]) rec[0] = 0;
 
-        for (int i=1; i<m; i++)
-            for (int j=1; j<n; j++)
+            for (int j=1; j<n; j++) {
                 if (!obstacleGrid[i][j]) 
-                    rec[i][j] = rec[i-1][j] + rec[i][j-1];
+                    rec[j] += rec[j-1];
+                else 
+                    rec[j] = 0;
+            }
+        }
 
-        return rec[m-1][n-1];
+        return rec[n-1];
     }
 };
