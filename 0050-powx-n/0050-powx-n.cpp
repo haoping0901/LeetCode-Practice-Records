a28518cfe38d1744b2e479d8689a7c0c94233e52
haoping0901 <144804410+haoping0901@users.noreply.github.com>
2024-07-10 01:06:05 +0800

    Time: 4 ms (20.57%), Space: 8.2 MB (22.16%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -10,16 +10,15 @@ public:
 
         int max_exp = 0;
         double ret = 1;
-        // 紀錄 x 在次方數為 2 的次方時的值
+        // Record the value of x when its exponent is a power of 2.
         vector<double> exp_table(32, 0);
         exp_table[0] = x;
 
-        for (auto cpy_n = ln; cpy_n > 1; cpy_n >>= 1, ++max_exp) {
+        for (auto cpy_n = ln; cpy_n > 1; cpy_n >>= 1, ++max_exp) 
             // n =      10
             // cpy_n:   10, 5, 2, 1
             // idx:     0,  1, 2, 3
             exp_table[max_exp+1] = exp_table[max_exp] * exp_table[max_exp];
-        }
 
         while (ln) {
             if (ln - (1LL << max_exp) >= 0) {
