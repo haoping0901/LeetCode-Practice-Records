5c242ea39561a210ba8958bd7b9e7beb3a851fd8
haoping0901 <144804410+haoping0901@users.noreply.github.com>
2024-07-10 01:12:59 +0800

    Time: 3 ms (37.99%), Space: 8 MB (73.92%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        bool is_negative = false;
         long long ln = n;
         if (n < 0) {
-            is_negative = true;
+            x = 1 / x;
             ln *= -1;
         }
 
-        int max_exp = 0;
         double ret = 1;
-        // Record the value of x when its exponent is a power of 2.
-        vector<double> exp_table(32, 0);
-        exp_table[0] = x;
-
-        for (auto cpy_n = ln; cpy_n > 1; cpy_n >>= 1, ++max_exp) 
-            // n =      10
-            // cpy_n:   10, 5, 2, 1
-            // idx:     0,  1, 2, 3
-            exp_table[max_exp+1] = exp_table[max_exp] * exp_table[max_exp];
 
         while (ln) {
-            if (ln - (1LL << max_exp) >= 0) {
-                ret *= exp_table[max_exp];
-                ln -= 1LL << max_exp;
-            }
-            
-            --max_exp;
+            if (ln & 0x1) ret *= x;
+
+            x *= x;
+            ln >>= 1;
         }
 
-        return is_negative ? 1 / ret : ret;
+        return ret;
     }
 };
