4394d21205872a7e3c915cac99c857c886e3d41e
haoping0901 <144804410+haoping0901@users.noreply.github.com>
2024-07-10 15:57:33 +0800

    Time: 2 ms (44.14%), Space: 10.1 MB (99.07%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -5,16 +5,14 @@ public:
         for (int idx = digits.size()-1; idx >= 0 && carry_in; --idx) {
             digits[idx] +=  carry_in;
 
-            if (digits[idx] > 9) 
-                digits[idx] %= 10;
+            if (digits[idx] == 10) 
+                digits[idx] = 0;
             else 
                 carry_in = 0;
         }
 
         if (carry_in) {
             digits.push_back(0);
-            for (int idx = digits.size()-1; idx > 0; --idx)
-                digits[idx] = digits[idx-1];
             digits[0] = carry_in;
         }
         return digits;
