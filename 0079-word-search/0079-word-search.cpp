fc1c4795d4f7094268dbd0b36ddf0a45ceffd5fb
haoping0901 <144804410+haoping0901@users.noreply.github.com>
2024-07-20 16:19:39 +0800

    Time: 627 ms (52.91%), Space: 11.2 MB (57.3%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,69 @@
+class Solution {
+    int rows_, cols_, str_len_;
+    string word_;
+
+    bool IsBorder(const int& r, const int& c) {
+        if (r >= 0 && r < rows_ && c >= 0 && c < cols_)
+            return false;
+        
+        return true;
+    }
+
+    bool NextStep(vector<vector<char>>& board, const int& r, const int& c, 
+                  int word_idx, vector<vector<bool>>& visited) {
+        bool exist = false;
+
+        if (board[r][c] == word_[word_idx]) {
+            ++word_idx;
+
+            if (word_idx == str_len_) return true;
+
+            if (!IsBorder(r+1, c) && !visited[r+1][c]) {
+                visited[r+1][c] = true;
+                exist |= NextStep(board, r+1, c, word_idx, visited);
+                visited[r+1][c] = false;
+            }
+            if (!IsBorder(r-1, c) && !visited[r-1][c]) {
+                visited[r-1][c] = true;
+                exist |= NextStep(board, r-1, c, word_idx, visited);
+                visited[r-1][c] = false;
+            }
+            if (!IsBorder(r, c-1) && !visited[r][c-1]) {
+                visited[r][c-1] = true;
+                exist |= NextStep(board, r, c-1, word_idx, visited);
+                visited[r][c-1] = false;
+            }
+            if (!IsBorder(r, c+1) && !visited[r][c+1]) {
+                visited[r][c+1] = true;
+                exist |= NextStep(board, r, c+1, word_idx, visited);
+                visited[r][c+1] = false;
+            }
+        } 
+
+        return exist;
+    }
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        rows_ = board.size();
+        cols_ = board[0].size();
+        str_len_ = word.length();
+        word_ = word;
+        vector<vector<bool>> visited(rows_, vector<bool>(cols_, false));
+
+        // DFS each elements
+        for (int c=0; c<cols_; ++c) {
+            for (int r=0; r<rows_; ++r) {
+                visited[r][c] = true;
+                if (NextStep(board, r, c, 0, visited))
+                    return true;
+
+                // reset the matrix recording visit info.
+                for (int v_r=0; v_r<rows_; ++v_r)
+                    for (int v_c=0; v_c<cols_; ++v_c)
+                        visited[v_r][v_c] = false;
+            }
+        }
+
+        return false;
+    }
+};
