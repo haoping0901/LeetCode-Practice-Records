d117acc39865a0197c1835db5576f381e794f904
haoping0901 <144804410+haoping0901@users.noreply.github.com>
2024-08-10 11:13:59 +0800

    Time: 235 ms (73.61%), Space: 93.6 MB (40.65%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,15 +1,14 @@
-static inline bool Cmp2EleLt(const vector<int>& a, const vector<int>& b) {
-    return a[1] < b[1];
-}
-
 class Solution {
+    static inline bool CmpSecEleLt(const vector<int>& a, const vector<int>& b) {
+        return a[1] < b[1];
+    }
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        // [1, 6], [2, 4], [4, 5]
-        // [2, 4], [4, 5], [1, 6]
+        // [1, 6], [2, 4], [3, 6], [4, 5]
+        // [2, 4], [4, 5], [1, 6], [4, 6]
 
         // sort intervals by end
-        sort(intervals.begin(), intervals.end(), Cmp2EleLt);
+        sort(intervals.begin(), intervals.end(), CmpSecEleLt);
 
         // Check whether the end of the current interval is greater than the 
         // start of the next interval.
