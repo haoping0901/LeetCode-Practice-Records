eb7a81d2d546942f3fe45ba450bf915973459f54
haoping0901 <144804410+haoping0901@users.noreply.github.com>
2025-01-22 19:19:45 +0800

    Time: 30 ms (39.6%), Space: 16.7 MB (62.22%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,47 +1,54 @@
 class Solution {
-    int m, n;
-    vector<vector<bool>> visited;
-public:
-    bool BFS(const vector<vector<char>>& grid, const int& i, const int&j) {
-        if (grid[i][j] == '0') return false;
+    int m_, n_;
 
-        // top
-        if (i-1 >= 0 && !visited[i-1][j]) {
-            visited[i-1][j] = true;
-            BFS(grid, i-1, j);
-        }
-
-        // down
-        if (i+1 < m && !visited[i+1][j]) {
-            visited[i+1][j] = true;
-            BFS(grid, i+1, j);
-        }
-
-        // left
-        if (j-1 >= 0 && !visited[i][j-1]) {
-            visited[i][j-1] = true;
-            BFS(grid, i, j-1);
-        }
-
-        // right
-        if (j+1 < n && !visited[i][j+1]) {
-            visited[i][j+1] = true;
-            BFS(grid, i, j+1);
-        }
-
-        return true;
+    bool IsValidCoordinate(const int& row, const int& col) const {
+        return row >= 0 && row < m_ && col >= 0 && col < n_;
     }
 
+    void DFS(const vector<vector<char>>& grid, 
+             vector<vector<bool>>& visited, const int& row, 
+             const int& col) const {
+        visited[row][col] = true;
+        // 判斷上下左右是否沒造訪過且為 1
+            // 是的話就遞迴尋找
+            // 是或不是都紀錄曾造訪過
+        // top
+        if (IsValidCoordinate(row-1, col) && !visited[row-1][col] && 
+                grid[row-1][col] == '1') {
+            DFS(grid, visited, row-1, col);
+        }
+        // down
+        if (IsValidCoordinate(row+1, col) && !visited[row+1][col] && 
+                grid[row+1][col] == '1') {
+            DFS(grid, visited, row+1, col);
+        }
+        // left
+        if (IsValidCoordinate(row, col-1) && !visited[row][col-1] && 
+                grid[row][col-1] == '1') {
+            DFS(grid, visited, row, col-1);
+        }
+        // right
+        if (IsValidCoordinate(row, col+1) && !visited[row][col+1] && 
+                grid[row][col+1] == '1') {
+            DFS(grid, visited, row, col+1);
+        }
+
+        return;
+    }
+public:
     int numIslands(vector<vector<char>>& grid) {
-        m = grid.size(), n = grid[0].size();
-        for (int i=0; i<m; i++) visited.push_back(vector(n, false));
+        // 遍歷所有點，對每個沒造訪過的路地用 DFS 走訪所有相連的陸地
+        // 走訪過程會記錄曾經造訪過，結束後增加代表陸地數量的值的變數
+        m_ = grid.size(), n_ = grid[0].size();
+        vector<vector<bool>> visited(m_, vector<bool>(n_, false));
+
         int cnt = 0;
-
-        for (int i=0; i<m; i++) {
-            for (int j=0; j<n; j++) {
-                if (visited[i][j]) continue;
-
-                if (BFS(grid, i, j)) cnt++;
+        for (int row=0; row<m_; ++row) {
+            for (int col=0; col<n_; ++col) {
+                if (!visited[row][col] && grid[row][col] == '1') {
+                    DFS(grid, visited, row, col);
+                    ++cnt;
+                }
             }
         }
 
