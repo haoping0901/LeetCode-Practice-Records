727c812cdd4d1d1a542107d90f98f6ee44e47844
haoping0901 <144804410+haoping0901@users.noreply.github.com>
2025-11-30 22:54:36 +0800

    Time: 0 ms (100%), Space: 8.8 MB (66.22%) - LeetHub

--- 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.cpp
+++ 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.cpp
@@ -6,24 +6,26 @@ public:
         // 當滿足 cond. 時不斷右移 r
         // 不滿足時就左移 l，且 reset r = l
         // 重複上方動作直到 l 碰到結尾
-        int l = 0, r;
+        int l = 0, r = 0;
         int n = s.length();
-        int cnt0, cnt1;
+        int cnt0 = 0, cnt1 = 0;
         int ret = 0;
 
-        for (l = 0; l < n; ++l) {
-            cnt0 = cnt1 = 0;
-            for (r = l; r < n; ++r) {
-                if (s[r] == '1')
-                    ++cnt1;
+        while (r < n) {
+            if (s[r] == '0')
+                ++cnt0;
+            else
+                ++cnt1;
+            
+            while (cnt0 > k && cnt1 > k) {
+                if (s[l++] == '0')
+                    --cnt0;
                 else
-                    ++cnt0;
-                
-                if (cnt1 > k && cnt0 > k)
-                    break;
-                
-                ++ret;
+                    --cnt1;
             }
+
+            ret += r - l + 1;
+            ++r;
         }
 
         return ret;
