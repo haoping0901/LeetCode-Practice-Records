3cfd16c12e0293f37273f87f222260c49b03c8dc
haoping0901 <144804410+haoping0901@users.noreply.github.com>
2026-02-08 15:16:27 +0800

    Time: 0 ms (100%), Space: 9.2 MB (56.33%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,44 +1,34 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
-        // string ans: rec part of string to be returned
-        // stack<string> st: rec part of string needed to be reversed
-        // Go through s, for each s[i]
-            // s[i] == '(': push string handling now tmp into stack if !tmp.empty()
-            // s[i] == ')': reverse tmp and concatenate to st.top() if !st.empty()
-                // otherwise, concatenate to ans
-            // s[i] == lower case letter
-                // tmp.push_back(s[i])
-        stack<string> st;
-        string ans, tmp;
+        int n = s.length();
+        vector<int> index(n);
+        stack<int> st;
+        string ans;
+        int idx = 0, step = 1;
 
-        for (const char ch : s) {
-            if (ch == '(') {
-                if (!tmp.empty()) {
-                    st.push(tmp);
-                    tmp.clear();
-                }
-
-                st.push("(");
-            } else if (ch == ')') {
-                reverse(tmp.begin(), tmp.end());
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else if (s[i] == ')') {
+                int j = st.top();
                 st.pop();
-
-                if (!st.empty() && st.top() != "(") {
-                    st.top() += tmp;
-                    tmp = st.top();
-                    st.pop();
-                } else if (st.empty()) {
-                    ans += tmp;
-                    tmp.clear();
-                }
-            } else { // ch == lower case English letters
-                tmp.push_back(ch);
+                index[i] = j;
+                index[j] = i;
             }
         }
 
-        return ans + tmp;
+        while (idx < n) {
+            if (s[idx] == '(' || s[idx] == ')') {
+                idx = index[idx];
+                step = -step;
+            } else {
+                ans.push_back(s[idx]);
+            }
+
+            idx += step;
+        }
+
+        return ans;
     }
 };
-// Input:   "ab(cd(e))"
-// Output:  "abedc"
